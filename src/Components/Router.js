9b2826fffc727fa35b8e4a991e15243ab4fb97d9
Mondaoporosis <kimwy1997@gmail.com>
2020-03-29 14:52:36 +0900

    Add Containers

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -6,9 +6,10 @@ import {
   Switch
 } from "react-router-dom";
 import Home from "Routes/Home";
-import TV from "Routes/Tv";
-import Search from "Routes/Search";
+import TV from "Routes/TV";
 import Header from "Components/Header";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 export default () => (
   <Router>
@@ -18,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
         <Route path="/search" exact component={Search} />
+        <Route path="/movie/:id" component={Detail}></Route>
+        <Route path="/show/:id" component={Detail}></Route>
         <Redirect from="*" to="/" />
         {/* if none of them match, go to home */}
       </Switch>
